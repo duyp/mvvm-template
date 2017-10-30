1c032f869eae81f6869f77a36b0887a7014b8706
Duy Pham <phamduy.uit@gmail.com>
2017-10-31 01:36:08 +0700

    navigate user profile at main drawer navigation

--- app/src/main/java/com/duyp/architecture/mvvm/ui/modules/main/MainDrawerHolder.java
+++ app/src/main/java/com/duyp/architecture/mvvm/ui/modules/main/MainDrawerHolder.java
@@ -1,6 +1,7 @@
 package com.duyp.architecture.mvvm.ui.modules.main;
 
 import android.app.Activity;
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -10,9 +11,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.duyp.androidutils.AlertUtils;
 import com.duyp.architecture.mvvm.R;
 import com.duyp.architecture.mvvm.data.local.user.UserManager;
 import com.duyp.architecture.mvvm.data.model.User;
+import com.duyp.architecture.mvvm.injection.qualifier.ActivityContext;
 import com.duyp.architecture.mvvm.ui.navigator.NavigatorHelper;
 import com.duyp.architecture.mvvm.ui.widgets.AvatarLayout;
 import com.duyp.architecture.mvvm.utils.AvatarLoader;
@@ -26,6 +29,7 @@ import javax.inject.Inject;
 
 public class MainDrawerHolder {
 
+    private final Context context;
     private final AvatarLoader avatarLoader;
     private final UserManager userManager;
     private final NavigatorHelper navigatorHelper;
@@ -35,21 +39,29 @@ public class MainDrawerHolder {
     private NavigationView extrasNav;
 
     @Inject
-    public MainDrawerHolder(AvatarLoader avatarLoader, UserManager userManager, NavigatorHelper navigatorHelper) {
+    public MainDrawerHolder(@ActivityContext Context context, AvatarLoader avatarLoader, UserManager userManager, NavigatorHelper navigatorHelper) {
         this.avatarLoader = avatarLoader;
         this.userManager = userManager;
         this.navigatorHelper = navigatorHelper;
+        this.context = context;
     }
 
     public void init(DrawerLayout drawerLayout) {
         drawer = drawerLayout;
         accountsNav = drawer.findViewById(R.id.accountsNav);
         extrasNav = drawer.findViewById(R.id.extrasNav);
+
+        extrasNav.getHeaderView(0).setOnClickListener(v -> {
+            navigateCurrentUserProfile();
+        });
         extrasNav.setNavigationItemSelectedListener(item -> {
             if (item.isChecked()) {
                 return false;
             }
             switch (item.getItemId()) {
+                case R.id.profile:
+                    navigateCurrentUserProfile();
+                    break;
                 case R.id.logout:
                     userManager.logout();
                     navigatorHelper.navigateLoginActivity(true);
@@ -58,6 +70,14 @@ public class MainDrawerHolder {
             drawer.closeDrawer(GravityCompat.START);
             return true;
         });
+    }
+
+    private void navigateCurrentUserProfile() {
+        if (userManager.getCurrentUser() != null) {
+            navigatorHelper.navigateUserProfile(userManager.getCurrentUser());
+        } else {
+            AlertUtils.showToastShortMessage(context, "Must login first!");
+        }
     }
 
     public void updateUser(User user) {
